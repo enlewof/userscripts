93e5b1008e0767de8495ef26e5453634bf845325
Allen Lew <allen@alew.org>
2012-04-02 20:03:55 -0700

    Forgot the function main() part; added versioning and an update url

--- betterworks-yelp-reviews.user.js
+++ betterworks-yelp-reviews.user.js
@@ -3,6 +3,8 @@
 // @description    Linking from Betterworks.com to Yelp search for reviews (code lifted from the Restaurant.com Yelp links script -http://userscripts.org/scripts/review/96836)
 // @namespace    http://allen.alew.org
 // @include        http://*.betterworks.com/*
+// @updateURL http://allen.alew.org/wp-content/uploads/2012/04/betterworks-yelp-reviews.user.js
+// @version 1.2
 // ==/UserScript==
 
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
@@ -18,16 +20,17 @@ function addJQuery(callback) {
 }
 
 // main function  of this userscript
-$(document).ready(function() {
-	$(".PerkTitle").each(function() {
-		var name = $("a", this).text().replace(/^\s+|\s+$/g, '');
-		$(this).append("<a href='http://www.yelp.com/search?find_desc=" + encodeURIComponent(name).replace("'", "%27") +"'><img src='http://media1.px.yelpcdn.com/static/201012161623981098/i/ico/favicon.ico'/></a>");
+function main() {
+	$(document).ready(function() {
+		$(".PerkTitle").each(function() {
+			var name = $("a", this).text().replace(/^\s+|\s+$/g, '');
+			$(this).append("<a href='http://www.yelp.com/search?find_desc=" + encodeURIComponent(name).replace("'", "%27") +"'><img src='http://media1.px.yelpcdn.com/static/201012161623981098/i/ico/favicon.ico'/></a>");
+		});
+		$(".PerkContact > h2").each(function() {
+			var name = $(this).text().replace(/^\s+|\s+$/g, '');
+			$(this).append("<a href='http://www.yelp.com/search?find_desc=" + encodeURIComponent(name).replace("'", "%27") +"'><img src='http://media1.px.yelpcdn.com/static/201012161623981098/i/ico/favicon.ico'/></a>");
+		});
 	});
-	$(".PerkContact > h2").each(function() {
-		var name = $(this).text().replace(/^\s+|\s+$/g, '');
-		$(this).append("<a href='http://www.yelp.com/search?find_desc=" + encodeURIComponent(name).replace("'", "%27") +"'><img src='http://media1.px.yelpcdn.com/static/201012161623981098/i/ico/favicon.ico'/></a>");
-	});
-});
-
+}
 // load jQuery and execute the main function
 addJQuery(main);
