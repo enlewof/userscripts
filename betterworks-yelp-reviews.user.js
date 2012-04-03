2d6fd79283364a6acdc3bf18cb4c157cd68e325a
Allen Lew <allen@alew.org>
2012-04-02 19:46:47 -0700

    Removing @require jquery line with script recommendations from http://erikvold.com/blog/index.cfm/2010/6/14/using-jquery-with-a-user-script for Google Chrome compatibility.

--- betterworks-yelp-reviews.user.js
+++ betterworks-yelp-reviews.user.js
@@ -3,9 +3,21 @@
 // @description    Linking from Betterworks.com to Yelp search for reviews (code lifted from the Restaurant.com Yelp links script -http://userscripts.org/scripts/review/96836)
 // @namespace    http://allen.alew.org
 // @include        http://*.betterworks.com/*
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js
 // ==/UserScript==
 
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+// main function  of this userscript
 $(document).ready(function() {
 	$(".PerkTitle").each(function() {
 		var name = $("a", this).text().replace(/^\s+|\s+$/g, '');
@@ -16,3 +28,6 @@ $(document).ready(function() {
 		$(this).append("<a href='http://www.yelp.com/search?find_desc=" + encodeURIComponent(name).replace("'", "%27") +"'><img src='http://media1.px.yelpcdn.com/static/201012161623981098/i/ico/favicon.ico'/></a>");
 	});
 });
+
+// load jQuery and execute the main function
+addJQuery(main);
